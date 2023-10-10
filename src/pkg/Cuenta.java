773f11627468fa09a632af30241b4d60442f14aa
MANUELgp47 <62995481+MANUELgp47@users.noreply.github.com>
2023-10-10 10:52:16 +0200

    TDD paso 1, crear pruebas

---
+++ src/pkg/Cuenta.java
@@ -0,0 +1,28 @@
+package pkg;
+
+public class Cuenta {
+
+	double saldo;
+	
+	public Cuenta(String numero, String titular, double saldo) {
+		super();
+		this.saldo = saldo;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+
+	public void Ingresar(int i) {
+		
+		
+	}
+
+
+
+}
